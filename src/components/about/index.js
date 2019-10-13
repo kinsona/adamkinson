096c215c0203b9508f1efc557bb5becf7d464c37
Adam Kinson <adam.kinson@gmail.com>
2019-10-12 22:27:32 -0400

    First updates (#1)
    * wip
    
    * dynamic image sizing
    
    * finish styling

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -4,21 +4,29 @@ import React from "react";
 import { StaticQuery, graphql } from 'gatsby'
 import Img from 'gatsby-image'
 
-function AboutSection({ fixedImage }) {
+function AboutSection({ headshot }) {
   return (
     <div className="AboutSection">
-      <Img className="AboutSection__Image" fixed={fixedImage} />
+      <Img className="AboutSection__Image" fluid={headshot} />
       <div className="AboutSection__Text">
         <p>
           <strong>Adam Kinson</strong> is a mission-driven software engineer living in West Hartford, CT.  Having
           transitioned into a tech career as his second act, he strongly believes in improving the accessibility of
-          technology as a means to address local economic, educational, and cultural needs.  His passion project,
-          Community Code, aims to empower people to solve issues in their own community with tech solutions.
+          technology as a means to address local economic, educational, and cultural needs.
         </p>
         <p>
           He currently works at Cigna building full-stack solutions using Python, Angular, Kubernetes, and AWS.  Previously,
           he worked at two growth-stage startups in Boston, Notarize and Alignable, developing user-facing
           features with React and Ruby on Rails.
+        </p>
+        <p>
+          Before pivoting to tech, Adam started his career as an actuary.  He spent over 8 years in P&C insurance
+          pricing home and auto products at two major Massachusetts-based companies, in a range of roles spanning
+          data delivery, business operations, and team management.
+        </p>
+        <p> 
+          He is available for hire for freelance projects or consulting.&nbsp;
+          <a className="AboutSection__Text__CTA" href="https://www.linkedin.com/in/adamkinson/">Reach out to learn more!</a>
         </p>
       </div>
     </div>
@@ -29,16 +37,16 @@ const AboutSectionWithImage = () => (
   <StaticQuery
     query={graphql`
       query {
-        placeholderImage: file(relativePath: { eq: "ajk.jpg" }) {
+        headshot: file(relativePath: { eq: "ajk.jpg" }) {
           childImageSharp {
-            fixed(width: 360) {
-              ...GatsbyImageSharpFixed
+            fluid(maxWidth: 360, srcSetBreakpoints: [ 100, 200, 360 ] ) {
+              ...GatsbyImageSharpFluid
             }
           }
         }
       }
     `}
-    render={data => <AboutSection fixedImage={data.placeholderImage.childImageSharp.fixed} />}
+    render={data => <AboutSection headshot={data.headshot.childImageSharp.fluid} />}
   />
 )
 
